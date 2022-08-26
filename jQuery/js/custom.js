7ab26a068b94b7b67b2013d4f0a0790af25f4d78
leonardo danier <leo.danier13@gmail.com>
2022-08-26 14:27:00 -0300

    intro jQuery

---
+++ jQuery/js/custom.js
@@ -0,0 +1,14 @@
+// instancia jquery e evita conflitos
+jQuery(function($){
+
+    let titulos = $('h4') // tag
+
+    let items = $('.featured-item') // class
+
+    let destaques = $('#featured') // id
+
+    titulos.first()
+
+})
+
+
